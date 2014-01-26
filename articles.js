598e4995f93dea44d242a8b33642ef45eedad422
Arpith Siromoney <arpith@feedreader.co>
2014-01-26 22:26:35 +0530

    Awssum to aws-sdk

--- articles.js
+++ articles.js
@@ -1,11 +1,9 @@
 var redis = require('redis').createClient()
 , crypto = require('crypto')
 , http = require('http')
-, awssum = require('awssum')
-, amazon = awssum.load('amazon/amazon')
-, amazonSettings = {'accessKeyId':'AKIAJGMWU75U3YTLXW7Q','secretAccessKey':'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','region':amazon.US_EAST_1}
-, S3 = awssum.load('amazon/s3').S3
-, s3 = new S3(amazonSettings)
+, AWS = require('aws-sdk')
+AWS.config.loadFromPath('./aws-config.json')
+var s3 = new AWS.S3({params:{Bucket:'articles.feedreader.co'}})
 exports.hash = function(article){
   return crypto.createHash('md5').update(article.description).digest('hex')
 }
@@ -13,7 +11,7 @@ exports.post = function(req,res){
   res.json({'success':true,'hash':exports.hash(req.body)})
 }
 exports.get = function(req,res){
-  s3.GetObject({BucketName:'articles.feedreader.co',ObjectName:req.params.hash},function(e,d){
+  s3.getObject({Key:req.params.hash},function(e,d){
     if (e) res.json({'success':false,'error':{'type':'S3 Error','message':"Couldn't get http://articles.feedreader.co/"+req.params.hash,'log':e}},500)
     else {
       var data = new Buffer(d.Body)
