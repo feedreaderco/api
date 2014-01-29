5f7e728603a7da4a0d0f4ae799ec25d9cae16001
Arpith Siromoney <arpith@feedreader.co>
2014-01-29 11:58:39 +0530

    Was using incorrect key for zrevrange

--- feeds.js
+++ feeds.js
@@ -55,7 +55,7 @@ exports.get = function(req,res) {
        , unionkeys = feedurls.map(function(feedkey){return 'articles:'+feedkey})
        redis.zunionstore(['articles:'+req.params.user,unionkeys.length].concat(unionkeys),function(e){
          if (e) res.json({'success':false,'error':{'type':'Redis Error','message':"Couldn't create article list for "+req.params.user,'log':e.message}},500)
-         else redis.zrevrange('articles:'+req.params.user+'/'+req.params.folder,0,-1,function(e,articleKeys){
+         else redis.zrevrange('articles:'+req.params.user,0,-1,function(e,articleKeys){
            if (e) res.json({'success':false,'error':{'type':'Redis Error','message':"Couldn't get article list for "+req.params.user}},500)
            else redis.del('articles:'+req.params.user,function(e){
              if (e) res.json({'success':false,'error':{'type':'Redis Error','message':"Couldn't delete article list for "+req.params.user}},500)
