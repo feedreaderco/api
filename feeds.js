7b315056e384a4b885c72732cba4542001b816c0
Arpith Siromoney <arpith@feedreader.co>
2014-02-01 12:09:11 +0530

    Update feeds.js

--- feeds.js
+++ feeds.js
@@ -119,7 +119,7 @@ exports.feed.get = function(req,res) {
       feedparser.on('readable', function() {
         var stream = this, article
         while (article = stream.read()) {
-          console.log(article.guid)
+          if (!article.guid) console.log(article)
           article.hash = hash(article)
           article.feedurl = feedrequested
           var body = JSON.stringify(article)
