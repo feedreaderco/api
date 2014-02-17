b585efd38a2347759df9097892f8246b29ca4fb2
Arpith Siromoney <arpith@feedreader.co>
2014-02-17 09:10:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.all('*', function(req,res,next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  if ('OPTIONS' == req.method) return res.send(200)
   next()
 })
 app.post('/:user/tokens/json',sessions.post)
