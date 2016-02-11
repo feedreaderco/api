753cd6e021f9ed0c4965f11f6e86c7744c4f226c
Arpith Siromoney <arpith@feedreader.co>
2016-02-11 17:38:35 +0530

    allow authorization header

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.all('*', function(req,res,next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, authorization")
   if ('OPTIONS' == req.method) return res.sendStatus(200)
   next()
 });
